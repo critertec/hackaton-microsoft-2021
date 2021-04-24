6d5c473156c159de25c0ee5863247845fff1e162
Juan León <juansele@gmail.com>
2021-04-24 16:12:18 -0500

    Initial UI kit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import logo from './assets/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Resuelve tus dudas</h1>
+      <h2>Ingresa al foro</h2>
+      <h3>Bienvenido</h3>
+
+      <div>
+        <button>Ir al evento</button>
+      </div>
+
+      <br/><br/>
+
+      <div>
+        <button>Semana 1</button>
+        <button>Semana 2</button>
+        <button>Semana 3</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
