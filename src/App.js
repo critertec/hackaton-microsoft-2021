2741be07b4ec2623e81b21cf9cd3bf87809dc3cb
Juan León <juansele@gmail.com>
2021-04-24 01:08:52 -0500

    deploy test again

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Critertec was here.
+          Critertec was here again.
         </p>
         <a
           className="App-link"
