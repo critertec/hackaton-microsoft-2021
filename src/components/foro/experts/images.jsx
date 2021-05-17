a376a0938014c824d7ce050411778b1ca6739cf0
Juan León <juansele@gmail.com>
2021-05-17 14:36:50 -0500

    Adds foro content

---
+++ src/components/foro/experts/images.jsx
@@ -0,0 +1,12 @@
+import david_rendon from "./david_rendon.png";
+import humberto_jaimes from "./humberto_jaimes.png";
+import luis_beltran from "./luis_beltran.png";
+import miguel_munoz from "./miguel_munoz.png";
+
+const images = {
+    david_rendon,
+    humberto_jaimes,
+    luis_beltran,
+    miguel_munoz
+}
+export default images;
--- src/components/foro/foro.jsx
+++ src/components/foro/foro.jsx
@@ -1,8 +1,42 @@
+import "./foro.css";
+import * as expertData from '../../experts.json';
+import images from "./experts/images";
+
 const Foro = () => {
   return (
-    <>
-      <h1>Foro</h1>
-    </>
+    <section>
+      <h1>Resuelve tus dudas</h1>
+      <p>
+        Si tienes preguntas respecto a algún proceso técnico, creativo o de la organización del hackathon, encuentra en el GitHub del hackathon la guía que necesitas. Contamos con un equipo de expertos dispuesto a asesorarte.
+      </p>
+
+      <h2>Ingresa al foro</h2>
+      <ol>
+        <li>
+         <b>Crea una cuenta en GitHub</b>, el proceso es agíl y lo puedes realizar desde aquí. En caso de que ya estés registrado, puedes acceder al foro usando tu cuenta.
+        </li>
+        <li>
+          Ingresa al foro oficial del Hackathon, en donde <b>encontrarás diferentes categorías en las cuales podrás realizar tus preguntas</b> y obtener ayuda de nuestro equipo de expertos.
+        </li>
+        <li>
+          Te invitamos a explorar las preguntas que otros integrantes hayan realizado, puede que allí <b>encuentres solución a tus dudas</b> o puedas ayudarlos a encontrar la respuesta a sus dudas.
+        </li>
+      </ol>
+      <button>ir al foro</button>
+
+      <h2>Conoce a los expertos</h2>
+      <div className="expert-container">
+        {expertData.default.map((expert) => { return(
+          <div className="expert">
+            <img src={images[expert.img]} alt="" />
+            <p>
+              <span>{expert.name}: </span>
+              {expert.content}
+            </p>
+          </div>
+        )})}
+      </div>
+    </section>
   );
 }
  
