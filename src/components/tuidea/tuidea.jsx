913efb56562f94cf7017a9b6a161154841674f84
kalvinche <juanjose.murillo@critertec.com>
2021-06-23 09:18:04 -0500

    Cambio de forms para : Tu idea

--- src/components/tuidea/tuidea.jsx
+++ src/components/tuidea/tuidea.jsx
@@ -6,7 +6,7 @@ const Idea = () => {
       <h1>Tu idea</h1>
       <iframe
         title="idea"
-        src="https://forms.office.com/Pages/ResponsePage.aspx?id=PTPrKun-zE6lChFwi8SscgaC4X80uQxGhplUhRMi3XZUQUdVNjRSN1NUN05URktQMkJSV1NQVko1TC4u&embed=true"
+        src="https://forms.office.com/Pages/ResponsePage.aspx?id=PTPrKun-zE6lChFwi8SscgaC4X80uQxGhplUhRMi3XZUMVdFM0hDRVo3TEhVR08yNTJCVk1LWlpaOC4u&embed=true" 
         frameborder= "0"
         marginWidth= "0"
         marginHeight= "0">
